393b438bdf35594dfc9240d598d4529fe5bd0f03
junghyeonsu <jung660317@naver.com>
2021-07-06 18:49:06 +0900

    주석 처리

--- src/components/scrollTimer/index.tsx
+++ src/components/scrollTimer/index.tsx
@@ -1,32 +1,32 @@
-import React, { useRef, useEffect, useState, useCallback } from 'react';
-import styled from 'styled-components';
+// import React, { useRef, useEffect, useState, useCallback } from 'react';
+// import styled from 'styled-components';
 
-import { useLayoutContext } from '#/contexts/LayoutContext';
+// import { useLayoutContext } from '#/contexts/LayoutContext';
 
-const Container = styled.article`
-  position: fixed;
-  font-size: 11px;
-  bottom: 37px;
-  right: 11px;
-  padding: 10px;
-  color: red;
-`;
+// const Container = styled.article`
+//   position: fixed;
+//   font-size: 11px;
+//   bottom: 37px;
+//   right: 11px;
+//   padding: 10px;
+//   color: red;
+// `;
 
-const ScrollTimer = () => {
-  const { isPossibleMove }: any = useLayoutContext(); // eslint-disable-line 
-  const [counter, setCounter] = useState(1.5);
+// const ScrollTimer = () => {
+//   const { isPossibleMove }: any = useLayoutContext(); // eslint-disable-line 
+//   const [counter, setCounter] = useState(1.5);
 
-  useEffect(() => {
-    const timer =
-      counter >= 0 &&
-      setInterval(() => setCounter(Math.round(counter - 0.1)), 100);
-    return () => {
-      clearInterval(timer);
-      setCounter(1.5);
-    };
-  }, [isPossibleMove, counter]);
+//   useEffect(() => {
+//     const timer =
+//       counter >= 0 &&
+//       setInterval(() => setCounter(Math.round(counter - 0.1)), 100);
+//     return () => {
+//       clearInterval(timer);
+//       setCounter(1.5);
+//     };
+//   }, [isPossibleMove, counter]);
 
-  return <Container>{counter}</Container>;
-};
+//   return <Container>{counter}</Container>;
+// };
 
-export default ScrollTimer;
+// export default ScrollTimer;
