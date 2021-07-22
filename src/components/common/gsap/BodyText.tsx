3ab4cc8025c5bbd1ebff50591402c4c948a90bf4
junghyeonsu <jung660317@naver.com>
2021-07-22 17:49:10 +0900

    gsap 모듈화

---
+++ src/components/common/gsap/BodyText.tsx
@@ -0,0 +1,37 @@
+import React, { useRef, useEffect, ReactChild, ReactChildren } from 'react';
+import styled from 'styled-components';
+import gsap from 'gsap';
+
+import { REGULAR_TEXT } from '#/constants';
+
+interface TextProps {
+  children?: ReactChild | ReactChildren | ReactChildren[] | ReactChild[];
+  delay: number;
+}
+
+const BodyTextContainer = styled.p`
+  position: relative;
+  font-family: ${REGULAR_TEXT};
+  font-size: 2vw;
+  line-height: 0.5vw;
+`;
+
+const BodyText = ({ children, delay }: TextProps) => {
+  const target = useRef(null);
+  useEffect(() => {
+    gsap.from(target.current, {
+      scrollTrigger: {
+        trigger: target.current,
+        toggleActions: 'restart reverse restart restart',
+      },
+      top: '-15%',
+      opacity: 0,
+      delay,
+      duration: 1.5,
+      ease: 'power3.out',
+    });
+  }, [delay]);
+  return <BodyTextContainer ref={target}>{children}</BodyTextContainer>;
+};
+
+export default BodyText;
---
+++ src/components/common/gsap/HeaderText.tsx
@@ -0,0 +1,38 @@
+import React, { useRef, useEffect, ReactChild, ReactChildren } from 'react';
+import styled from 'styled-components';
+import gsap from 'gsap';
+
+import { BOLD_TEXT } from '#/constants';
+
+interface TextProps {
+  children?: ReactChild | ReactChildren | ReactChildren[] | ReactChild[];
+  delay: number;
+}
+
+const HeaderTextContainer = styled.p`
+  position: relative;
+  font-family: ${BOLD_TEXT};
+  font-size: 5vw;
+  letter-spacing: 2px;
+  line-height: 2vw;
+`;
+
+const HeaderText = ({ children, delay }: TextProps) => {
+  const target = useRef(null);
+  useEffect(() => {
+    gsap.from(target.current, {
+      scrollTrigger: {
+        trigger: target.current,
+        toggleActions: 'restart reverse restart restart',
+      },
+      top: '-15%',
+      opacity: 0,
+      delay,
+      duration: 2,
+      ease: 'power3.out',
+    });
+  }, [delay]);
+  return <HeaderTextContainer ref={target}>{children}</HeaderTextContainer>;
+};
+
+export default HeaderText;
