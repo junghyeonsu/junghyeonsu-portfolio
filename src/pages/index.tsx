62ff1da975b11095d92292e43b6499d7a0c33414
junghyeonsu <jung660317@naver.com>
2021-08-31 17:37:47 +0900

    파일 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,15 +25,23 @@ export default function Main() {
     <div>
       <Head>
         <title>Hyeonsu Jung</title>
+        <meta name="description" content="Hyeonsu Jung's portfolio" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, minimum-scale=1 maximum-scale=1, user-scalable=no"
+        />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
+      <body>
         <Section>
           <Introduction />
           <Skill />
           <Experience />
           <Contact />
         </Section>
-      </div>
+        <script src="https://unpkg.com/smoothscroll-polyfill/dist/smoothscroll.min.js" />
+        <script src="https://unpkg.com/smoothscroll-anchor-polyfill" />
+      </body>
       <ScrollGauge />
       {/* {!isPossibleMove ? <ScrollTimer /> : ''}
       {!isPossibleMiniMove ? <MiniScrollTimer /> : ''} */}
