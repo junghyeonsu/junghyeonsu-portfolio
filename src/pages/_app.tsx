d930d4d880451965b7ac3fde16a133684cc15d50
정현수 <jung660317@naver.com>
2022-01-21 21:03:41 +0900

    refactor: 경험, 스킬 섹션만 생성

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,9 @@ import { ThemeProvider } from 'styled-components';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 import smoothscroll from 'smoothscroll-polyfill';
-import disableScroll from 'disable-scroll';
 
 import GlobalStyle from '#/styles/globalStyles';
 import LayoutProvider from '#/contexts/LayoutContext';
-import ExperienceProvider from '#/contexts/ExperienceContext';
 import ContactCardProvider from '#/contexts/ContactCardContext';
 import { theme } from '#/styles/theme';
 
@@ -22,13 +20,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <LayoutProvider>
-        <ExperienceProvider>
-          <ContactCardProvider>
-            <GlobalStyle />
-            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-            <Component {...pageProps} />
-          </ContactCardProvider>
-        </ExperienceProvider>
+        <ContactCardProvider>
+          <GlobalStyle />
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          <Component {...pageProps} />
+        </ContactCardProvider>
       </LayoutProvider>
     </ThemeProvider>
   );
