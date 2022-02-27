0a97f220b4c9f2474d99cc527521c361da75f219
정현수 <jung660317@naver.com>
2022-02-27 23:32:18 +0900

    feat: contentOfTable 생성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import Head from 'next/head';
 import styled from 'styled-components';
 
-import Introduction from '#/components/introduction';
-import Experience from '#/components/experience';
+import Introduction from '#/components/Introduction';
+import Experience from '#/components/Experience';
+import ContentOfTable from '#/components/ContentOfTable';
 
 const Section = styled.section`
   position: relative;
@@ -17,16 +18,14 @@ export default function Main() {
       <Head>
         <title>정현수 포트폴리오</title>
         <meta name="description" content="Hyeonsu Jung's portfolio" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, minimum-scale=1 maximum-scale=1, user-scalable=no"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1 maximum-scale=1, user-scalable=no" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
         <Section>
           <Introduction />
           <Experience />
+          {/* <ContentOfTable /> */}
         </Section>
       </body>
     </div>
