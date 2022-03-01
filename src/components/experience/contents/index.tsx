d0eb9e7f22c818ff9f812f074e22be6ddf55f531
정현수 <jung660317@naver.com>
2022-03-01 21:19:48 +0900

    feat: 우테코, 넥스트스텝 교육 추가

--- src/components/experience/contents/index.tsx
+++ src/components/experience/contents/index.tsx
@@ -1,5 +1,7 @@
 /* Education */
 export { default as Univercity } from './educations/Univercity';
+export { default as NextStepCleanCodeWithJavaScript } from './educations/NextStepCleanCodeWithJavaScript';
+export { default as WooahanTechCource } from './educations/WooAHanTechCource';
 
 /* Works */
 export { default as Hayanmind } from './works/Hayanmind';
