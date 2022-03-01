16ca678baa2e471e6be68f7b4b29cd47be507ad8
정현수 <jung660317@naver.com>
2022-03-01 20:23:59 +0900

    chore: meta 태그 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,15 @@ export default function Main() {
     <div>
       <Head>
         <title>정현수 포트폴리오</title>
-        <meta name="description" content="Hyeonsu Jung's portfolio" />
+        <meta property="og:url" content="https://hyeonsu-jung.vercel.app/" />
+        <meta property="og:image" content="https://github.com/junghyeonsu/hyeonsu-jung/blob/main/public/hyeonsu.jpeg?raw=true" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="정현수 포트폴리오" />
+        <meta property="og:title" content="정현수 포트폴리오" />
+        <meta property="og:description" content="최고의 프론트엔드 개발자를 꿈꾸는 정현수의 포트폴리오입니다." />
+        <meta property="og:locale" content="ko" />
+        <meta name="description" content="최고의 프론트엔드 개발자를 꿈꾸는 정현수의 포트폴리오입니다." />
+        <meta name="google-site-verification" content="03tUkcTUQFs2twM6WNUdGdFy78xmlW1U0EfmTfRGCS8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1 maximum-scale=1, user-scalable=no" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
