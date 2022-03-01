7afccab5aa55915c7d8689085f3a4d190825fb74
정현수 <jung660317@naver.com>
2022-03-01 21:00:56 +0900

    feat: 대학교 추가

--- src/components/experience/contents/index.tsx
+++ src/components/experience/contents/index.tsx
@@ -1,14 +1,21 @@
+/* Education */
+export { default as Univercity } from './educations/Univercity';
+
+/* Works */
 export { default as Hayanmind } from './works/Hayanmind';
 export { default as SatrecI } from './works/SatrecI';
 
+/* Projects */
 export { default as Portfolio } from './projects/Portfolio';
 export { default as ReactSeasonComponent } from './projects/ReactSeasonComponent';
 export { default as FindDifferentColor } from './projects/FindDifferentColor';
 
+/* Shares */
 export { default as Velog } from './shares/Velog';
 export { default as Tistory } from './shares/Tistory';
 export { default as YouTube } from './shares/YouTube';
 
+/* Open Sources */
 export { default as Kubernetes } from './openSources/Kubernetes';
 export { default as ReactContribute } from './openSources/ReactContribute';
 export { default as GoogleWebFundamentals } from './openSources/GoogleWebFundamentals';
