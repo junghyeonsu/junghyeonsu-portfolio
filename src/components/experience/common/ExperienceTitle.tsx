86597132de0fb381846f735f6471ead916269e8f
정현수 <jung660317@naver.com>
2022-03-13 20:46:14 +0900

    style: TableOfContents 스타일 변경
    - Experience 컴포넌트안에서 Toc 컴포넌트를 감싸주는 컴포넌트를 생성해서 관리

--- src/components/experience/common/ExperienceTitle.tsx
+++ src/components/experience/common/ExperienceTitle.tsx
@@ -2,44 +2,43 @@ import React from 'react';
 import styled from 'styled-components';
 import YellowPoint from '#/components/YellowPoint';
 
-import { BOLD_TEXT, DESKTOP_CONTENT_WIDTH } from '#/constants';
+import { BOLD_TEXT } from '#/constants';
 
 const StyledExperienceTitle = styled.h1`
-  width: ${DESKTOP_CONTENT_WIDTH};
+  width: 75vw;
   font-size: 70px;
   font-family: ${BOLD_TEXT};
   margin: 0;
 
   @media ${props => props.theme.desktop} {
-    width: ${DESKTOP_CONTENT_WIDTH};
+    width: 75vw;
   }
 
   @media ${props => props.theme.tablet} {
-    width: 80vw;
+    width: 90vw;
   }
 
   @media ${props => props.theme.mobile} {
-    width: 90vw;
+    width: 80vw;
     font-size: 50px;
   }
 `;
 
 const Divide = styled.div`
-  width: ${DESKTOP_CONTENT_WIDTH};
   height: 6.5px;
   margin-top: 30px;
   background-color: #f8cd07;
 
   @media ${props => props.theme.desktop} {
-    width: ${DESKTOP_CONTENT_WIDTH};
+    width: 75vw;
   }
 
   @media ${props => props.theme.tablet} {
-    width: 80vw;
+    width: 90vw;
   }
 
   @media ${props => props.theme.mobile} {
-    width: 90vw;
+    width: 80vw;
   }
 `;
 
