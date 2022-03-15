227ba44c18615234410056ab638e86ed34e1dc45
정현수 <jung660317@naver.com>
2022-03-15 14:51:01 +0900

    feat: SecondIndentExplain 생성 및 NextStep 내용 보강
    - SecondIndentExplain 컴포넌트 생성해서 한 칸 들어가는 설명 컴포넌트 생성
    - Nextstep 온보딩 미션 PR 링크 첨부

--- src/components/experience/common/index.tsx
+++ src/components/experience/common/index.tsx
@@ -1,4 +1,5 @@
 export { default as Explain } from './Explain';
+export { default as SecondIndentExplain } from './SecondIndentExplain';
 export { default as Link } from './Link';
 export { default as InlineLinkText } from './InlineLinkText';
 export { default as ExperienceTitle } from './ExperienceTitle';
--- src/components/experience/contents/educations/NextStepCleanCodeWithJavaScript.tsx
+++ src/components/experience/contents/educations/NextStepCleanCodeWithJavaScript.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as Styled from '../../styled';
-import { Explain, InlineLinkText } from '../../common';
+import { Explain, SecondIndentExplain, InlineLinkText } from '../../common';
 
 const NextStepCleanCodeWithJavaScript = () => {
   return (
@@ -8,11 +8,15 @@ const NextStepCleanCodeWithJavaScript = () => {
       <Styled.ExperienceCardTopContainer>
         <Styled.ExperienceCardTitleType>교육</Styled.ExperienceCardTitleType>
         <Styled.ExperienceCardTitle id="next-step-javascript-clean-code">Clean Code with JavaScript 2기 (진행중)</Styled.ExperienceCardTitle>
+        <Styled.ExperienceCardPeriod>2022/3/8 - 2022/4/26</Styled.ExperienceCardPeriod>
         <Styled.ExperienceCardExplainContainer>
           <Explain>
             <InlineLinkText href="https://edu.nextstep.camp/">NEXTSTEP</InlineLinkText>에서 주최
           </Explain>
-          <Explain>2022/3/8 - 2022/4/26</Explain>
+          <Explain>온보딩 미션 진행 (2022/3/8 - 2022/3/15)</Explain>
+          <SecondIndentExplain>
+            <InlineLinkText href="https://github.com/next-step/js-calculator/pull/97">온보딩 미션 PR</InlineLinkText>
+          </SecondIndentExplain>
         </Styled.ExperienceCardExplainContainer>
       </Styled.ExperienceCardTopContainer>
     </Styled.ExperienceCard>
