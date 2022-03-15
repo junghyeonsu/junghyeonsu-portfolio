b89cba1b95e693e95b5009c05549832c4126e6e2
정현수 <jung660317@naver.com>
2022-03-15 14:51:01 +0900

    feat: SecondIndentExplain 생성 및 NextStep 내용 보강
    - SecondIndentExplain 컴포넌트 생성해서 한 칸 들어가는 설명 컴포넌트 생성
    - Nextstep 온보딩 미션 PR 링크 첨부

---
+++ src/components/experience/common/SecondIndentExplain.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import FiberManualRecordOutlinedIcon from '@material-ui/icons/FiberManualRecordOutlined';
+
+const ExplainContainer = styled.div`
+  position: relative;
+  left: 30px;
+  display: flex;
+`;
+
+const ExperienceCardExplain = styled.div`
+  font-size: 16px;
+
+  @media ${props => props.theme.mobile} {
+    font-size: 14px;
+  }
+`;
+
+const ExplainCircleIcon = styled(FiberManualRecordOutlinedIcon)`
+  margin-right: 10px;
+  color: rgb(255, 174, 0);
+`;
+
+const SecondIndentExplain = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ExplainContainer>
+      <ExplainCircleIcon fontSize="small" />
+      <ExperienceCardExplain>{children}</ExperienceCardExplain>
+    </ExplainContainer>
+  );
+};
+
+export default SecondIndentExplain;
