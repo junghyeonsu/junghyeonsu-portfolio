6467e7973bcbaae816d3e92fbaf5e941f7ae735e
정현수 <jung660317@naver.com>
2022-04-10 13:36:02 +0900

    docs: react-season-component 내용 추가

--- src/components/ExperienceContainer/contents/projects/ReactSeasonComponent.tsx
+++ src/components/ExperienceContainer/contents/projects/ReactSeasonComponent.tsx
@@ -5,7 +5,7 @@ import EXPLAIN from './explains';
 
 const LINKS = [
   {
-    text: 'React npm 배포 관련 블로그 포스팅',
+    text: 'React NPM 배포 관련 블로그 포스팅',
     href: 'https://velog.io/@junghyeonsu/NPM-%EB%B0%B0%ED%8F%AC-%EC%96%B4%EB%A0%B5%EC%A7%80-%EC%95%8A%EC%95%84%EC%9A%94-with-%ED%85%8C%EC%98%A4-%EA%B5%AC%EA%B8%80-%EC%8A%A4%ED%94%84%EB%A6%B0%ED%8A%B8-4%EA%B8%B0',
   },
   {
@@ -63,7 +63,8 @@ const ReactSeasonComponent = () => {
           <InlineLinkText href="https://velog.io/@teo/google-sprint-4">테오 구글 스프린트 4기</InlineLinkText>에 참여
         </Explain>
         <Explain>디자인 1명, 개발자 7명에서 프로젝트 리더로 시간 조율 및 회의 진행 담당</Explain>
-        <Explain>npm 배포 과정 및 3개의 컴포넌트 중 Time 컴포넌트 개발 담당</Explain>
+        <Explain>Time, Season, Weather 컴포넌트 중 Time 컴포넌트 개발 담당</Explain>
+        <Explain>tsc를 이용한 컴파일 및 NPM 배포</Explain>
       </Styled.ExperienceCardExplainContainer>
       <Links links={LINKS} />
       <Skills skills={SKILLS} />
