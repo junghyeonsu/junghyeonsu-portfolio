56c8b5442d664ae4cc24c74ad55c043bd11392a2
정현수 <jung660317@naver.com>
2022-04-10 13:41:28 +0900

    style: Footer border top 제거

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Container = styled.footer`
   align-items: center;
   width: 100%;
   height: 220px;
-  border-top: 1px solid rgb(231, 231, 231);
+
   background-color: rgb(245, 245, 245);
   color: #858585;
   z-index: 5;
