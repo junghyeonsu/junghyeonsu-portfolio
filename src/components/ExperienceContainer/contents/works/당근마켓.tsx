358f68f889afdd561b820730522537cd99662cf0
junghyeonsu <jung660317@naver.com>
2022-08-11 02:25:54 +0900

    feat: 당근마켓 내용 추가

--- src/components/ExperienceContainer/contents/works/당근마켓.tsx
+++ src/components/ExperienceContainer/contents/works/당근마켓.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import * as Styled from '../../styled';
-import { /* Explain, Links, */ InlineLinkText } from '../../common';
+import { Links, InlineLinkText, Explain } from '../../common';
 
-// const LINKS = [
-//   {
-//     href: 'https://redkiwiapp.com/ko/b2b',
-//     text: 'B2B 프로그램 소개 페이지',
-//   },
-//   {
-//     href: 'https://velog.io/@junghyeonsu/회고록-5월-및-인턴-회고',
-//     text: '인턴 회고록 블로그 포스트 (어려웠던 점, 느낀 점 등등 정리)',
-//   },
-// ];
+const LINKS = [
+  {
+    href: 'https://github.com/daangn/codepocket',
+    text: 'codepocket',
+  },
+  {
+    href: 'https://junghyeonsu-dev.vercel.app/posts/Daangn-intern-middle-retrospect',
+    text: '인턴 중간 회고록',
+  },
+];
 
 const 당근마켓 = () => {
   return (
@@ -20,11 +20,12 @@ const 당근마켓 = () => {
       <Styled.ExperienceCardTitle id="당근마켓">
         🥕 <InlineLinkText href="https://team.daangn.com/">당근마켓</InlineLinkText>
       </Styled.ExperienceCardTitle>
-      <Styled.ExperienceCardPeriod>2022년 6월 27일 - 2022년 8월 26일(진행중)</Styled.ExperienceCardPeriod>
-
-      <Styled.ExperienceCardExplainContainer />
-
-      {/* <Links links={LINKS} /> */}
+      <Styled.ExperienceCardPeriod>2022년 6월 27일 - 2022년 8월 26일 (진행중)</Styled.ExperienceCardPeriod>
+      <Styled.ExperienceCardExplainContainer>
+        <Explain>사내 개발자들의 DX를 향상시켜주는 프론트엔드코어팀 소속</Explain>
+        <Explain>흩어진 팀의 중복된 노력을 줄여주기 위한 프로젝트 Codepocket 진행</Explain>
+      </Styled.ExperienceCardExplainContainer>
+      <Links links={LINKS} />
     </Styled.ExperienceCard>
   );
 };
